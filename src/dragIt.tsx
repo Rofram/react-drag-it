118a565c9d1adbeca870b4a61ce3e05b9419d55a
Rodrigo de França <howdrigo.fr@gmail.com>
2022-03-30 17:55:01 -0300

    testando lib

--- src/dragIt.tsx
+++ src/dragIt.tsx
@@ -3,16 +3,21 @@ import { useRef } from 'react'
 interface DragItProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function DragIt({ children, ...props }: DragItProps) {
-  const divRef = useRef<HTMLDivElement | null>(null)
+  const divRef = useRef<HTMLDivElement>(null)
   let dragging = false
   let containerPosition = { x: 0, y: 0 }
   let mouseOffset = { x: 0, y: 0 }
 
+  console.log(dragging)
+
   function onMove(e: React.MouseEvent<HTMLDivElement>) {
     if (dragging) {
-      divRef.current!.style.left = `${(e.clientX + containerPosition.x) - mouseOffset.x}px`
-      divRef.current!.style.top = `${(e.clientY + containerPosition.y) - mouseOffset.y}px`
+      if (divRef.current) {
+        divRef.current.style.left = `${(e.clientX + containerPosition.x) - mouseOffset.x}px`
+        divRef.current.style.top = `${(e.clientY + containerPosition.y) - mouseOffset.y}px`
+      }
     }
+    console.log(`onMove: ${e.clientX} ${e.clientY}`)
   }
 
   function onMouseDown(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
@@ -21,6 +26,7 @@ export function DragIt({ children, ...props }: DragItProps) {
     mouseOffset.y = e.clientY
     containerPosition.x = divRef.current?.offsetLeft ?? 0
     containerPosition.y = divRef.current?.offsetTop ?? 0
+    console.log(`onMouseDown: ${e.clientX} ${e.clientY}`)
   }
 
   return (
